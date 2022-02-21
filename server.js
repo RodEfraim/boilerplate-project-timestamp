90e3aa7c6b748403641e209b24804306cda7067d
RodEfraim <refraim@ucsd.edu>
2022-02-20 17:10:36 -0800

    Added enpoints to return desired date.

--- server.js
+++ server.js
@@ -18,12 +18,74 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
-// your first API endpoint... 
+/**
+ * Testing API endpoint.
+ */
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({greeting: 'hello World'});
 });
 
+// Helper function to validate date.
+function isValidDate(d) {
+  return d instanceof Date && !isNaN(d);
+}
+
+/**
+ * If invalid date, then assume it is of unix format and convert that into a date 
+ * If the unix format fails to convert into a valid date, the return the following: 
+ * return res.json({error: "Invalid Date"}). Input examples shown below.
+ * 
+ * Date input
+ * http://localhost:61142/api/Fri, 25 Dec 2015 00:00:00 GMT 
+ * or
+ * Date unix format input
+ * http://localhost:61142/api/1451001600000
+ * or
+ * Invalid input
+ * http://localhost:61142/api/1213nhjbdsh
+ * 
+ */
+app.get("/api/:date", function (req, res) {
+
+  var echoString = req.params.date;
+  var timestamp = Date.parse(echoString);
+  var inputDate = null 
+
+  if (isNaN(timestamp) == false) {
+    inputDate = new Date(timestamp);
+  }
+  else{
+
+    inputDate = new Date(echoString * 1);
+
+    // If true then it is of unix format.
+    if(isValidDate(inputDate)){
+      console.log("Valid unix format.")
+    }
+    else{
+      console.log("Invalid date format.")
+      return res.json({error: "Invalid Date"});
+    }
+  }
+
+  console.log("echoString: " + echoString);
+  console.log("inputDate: " + inputDate);
+  console.log("inputDate.toUTCString(): " + inputDate.toUTCString());
+
+  var inputDateUnixFormat = inputDate.getTime();
+  console.log("inputDateUnixFormat: " + inputDateUnixFormat);
+
+  res.send({"unix": inputDateUnixFormat, "utc": inputDate.toUTCString()});
+});
+
+/**
+ * When the user does not enter a date, return the current date.
+ */
+app.get("/api", function(req, res){
+  var d = new Date();
+  console.log("Empty string was entered!");
+  res.send({"unix": d.getTime(), "utc": d.toUTCString()});
+});
 
 
 // listen for requests :)
